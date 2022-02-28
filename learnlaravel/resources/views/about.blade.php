aa0f01ae5497358cbbf7fbf96f14e9ecaf23cd99
siddharthmishra95 <siiid.mishra@gmail.com>
2022-02-28 16:00:48 +0530

    Laravel Route Page

--- learnlaravel/resources/views/about.blade.php
+++ learnlaravel/resources/views/about.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>About</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -399,7 +399,7 @@
 </head>
 
 <body class="antialiased">
-  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+  <div class="relative flex items-top justify-center min-h-screen  sm:items-center sm:pt-0">
     @if (Route::has('login'))
     <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
       @auth
@@ -418,7 +418,7 @@
       <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
 
       </div>
-      <h1> Home Page </h1>
+      <h1> About Page </h1>
 
       <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
         <div class="grid grid-cols-1 md:grid-cols-2">
